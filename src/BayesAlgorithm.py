d2d7a0b99fe8be4319cd5c6d5640d95a1e293bc5
Bryson Steck <brysonsteck@protonmail.com>
2023-04-06 18:57:14 -0600

    fixed array stuff

--- src/BayesAlgorithm.py
+++ src/BayesAlgorithm.py
@@ -6,13 +6,13 @@ import random
 def bayes():
     WentOrNot = [[],[],[],[],[],[],[]]
     MOCK_DATA = { # how busy % wise the gym is for each hour in 24hrs
-        'Sunday' : [np.random.randint(1, 101, size=24)],
-        'Monday' : [np.random.randint(1, 101, size=24)],
-        'Tuesday' : [np.random.randint(1, 101, size=24)],
-        'Wednesday' : [np.random.randint(1, 101, size=24)],
-        'Thursday' : [np.random.randint(1, 101, size=24)],
-        'Friday' : [np.random.randint(1, 101, size=24)],
-        'Saturday' : [np.random.randint(1, 101, size=24)]
+        'Sunday' : np.random.randint(1, 101, size=24),
+        'Monday' : np.random.randint(1, 101, size=24),
+        'Tuesday' : np.random.randint(1, 101, size=24),
+        'Wednesday' : np.random.randint(1, 101, size=24),
+        'Thursday' : np.random.randint(1, 101, size=24),
+        'Friday' : np.random.randint(1, 101, size=24),
+        'Saturday' : np.random.randint(1, 101, size=24)
     }
 
     # P(A|B) = P(B|A) * P(A) / P(B)
@@ -20,30 +20,35 @@ def bayes():
     # B = the time of day and the week (how busy % wise AKA MOCK_DATA)
     index = 0
     for key, value in MOCK_DATA.items():
-        for capacityPercentage in value:
-            for hourPercentage in capacityPercentage:
-                if hourPercentage > 80:
-                    WentOrNot[index].append(False)
-                elif hourPercentage > 50 or hourPercentage < 80:
-                    WentOrNot[index].append(random.randint(0, 1) == 1)
-                else: # hourPercentage < 50%
-                    WentOrNot[index].append(True)
+        for hourPercentage in value:
+            if hourPercentage > 80:
+                WentOrNot[index].append(False)
+            elif hourPercentage > 50 or hourPercentage < 80:
+                WentOrNot[index].append(random.randint(0, 1) == 1)
+            else: # hourPercentage < 50%
+                WentOrNot[index].append(True)
         index += 1
 
     index = 0
-
     for key, value in MOCK_DATA.items():
+        index2 = 0
+
         print(key)
         print(value)
-        for capacityPercentage in value:
-            index2 = 0
-            for hourPercentage in capacityPercentage:
-                print(WentOrNot[index][index2])
-                if WentOrNot[index][index2]:
-                    hourPercentage += 1
-                    value[index2] = hourPercentage
-                index2 += 1
-            index += 1
+        for hour in value:
+            if WentOrNot[index][index2]:
+                hour += 1
+                value[index2] = hour
+            index2+=1
+        # for capacityPercentage in value:
+        #     print(WentOrNot[index][index2])
+        #     if WentOrNot[index][index2]:
+        #         hourPercentage += 1
+        #         value[index] = hourPercentage
+        #     index += 1
         print(key)
         print(value)
-        sys.exit(0)
+        index+=1
+        sys.exit()
+
+bayes()
