b63939a9a6b964c1d21540c5ab399b056bfb1441
Jenna Cioffi <JMCioffi4@gmail.com>
2023-04-13 18:39:21 -0600

    cleaned up typo

--- src/main.py
+++ src/main.py
@@ -7,12 +7,11 @@ try:
     while(True): # keep running till user exits
         print('---------------------------')
         userInput = input("**MENU**\n\n"
-            "1 = Optimal Stopping\n"
-            "2 = Bayes Theorem\n"
-            "3 = Scheduling\n"
-            "CTRL+C` = Exit \n\n"
+            "1 : Optimal Stopping\n"
+            "2 : Bayes Theorem\n"
+            "3 : Scheduling\n"
+            "CTRL+C : Exit \n\n"
             ">> ")
-
         print('---------------------------')
         if userInput.isdigit():
             userInput = int(userInput)
